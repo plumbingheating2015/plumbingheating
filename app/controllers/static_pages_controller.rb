c3374948a5fd017968e00238754ff80650bf5e66
plumbing <plumbingheating2015@gmail.com>
2015-03-23 18:38:39 +0530

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def services
+  end
+
+  def schedule
+  end
+
+  def specials
+  end
+
+  def contact
+  end
+
+  def blog
+  end
+
+  def help
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Plumbingheating::Application.config.session_store :cookie_store, key: '_plumbingheating_session'
