73e1b34fd257a1463cee410a48063a469c6e0ed5
plumbingheating <plumbingheating2015@gmail.com>
2015-05-02 16:52:58 +0530

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,5 @@ Plumbingheating::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'http://www.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.asset_host = "http://www.calgaryserviceplumbers.com/"
 end
